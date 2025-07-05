034dd56aedbe61c8580dc23df0cfbff4fa806db6
Chen <12373718+CKgiteehub@user.noreply.gitee.com>
2025-07-05 15:59:33 +0800

    检测算法准确率测试

--- Util/Path.py
+++ Util/Path.py
@@ -62,20 +62,48 @@ def getFPsByEndwith(rootPath: str, endwith: str) -> List[str]:
         filePaths.append(path)
     return filePaths
 
-def getDPsByName(rootPath: str, dirName: str) -> List[str]:
+def getFPsByName(rootPath: str, fileName: str, strict: bool= False, first: bool= False) -> List[str]:
+    """
+    获取根目录下指定文件路径列表
+
+    Args:
+        rootPath (str): 根目录
+        fileName (str): 指定文件名称
+        strict (bool, optional): 是否严格匹配名称. Defaults to False.
+        first (bool, optional): 是否只获取第一个匹配项. Defaults to False.
+
+    Returns:
+        List[str]: 文件路径列表
+    """
+    filePaths = []
+    for filePath in getFilePaths(rootPath):
+        # 不包含 或 严格且不相同 的跳过
+        if not fileName in re.split(r"[\\/]", filePath)[-1] or (strict and not fileName == re.split(r"[\\/]", filePath)[-1]):
+            continue
+        filePaths.append(filePath)
+        # 是否只获取第一个匹配项
+        if first: break
+    return filePaths
+
+def getDPsByName(rootPath: str, dirName: str, strict: bool= False, first: bool= False) -> List[str]:
     """
     获取根目录下指定文件夹路径列表
 
     Args:
         rootPath (str): 根目录
-        dirName (str): 指定文件夹
+        dirName (str): 指定文件夹名称
+        strict (bool, optional): 是否严格匹配名称. Defaults to False.
+        first (bool, optional): 是否只获取第一个匹配项. Defaults to False.
 
     Returns:
         List[str]: 文件夹路径列表
     """
     dirPaths = []
     for dirPath in getDirPaths(rootPath):
-        if not re.split(r"[\\/]", dirPath)[-1] == dirName:
+        # 不包含 或 严格且不相同 的跳过
+        if not dirName in re.split(r"[\\/]", dirPath)[-1] or (strict and not dirName == re.split(r"[\\/]", dirPath)[-1]):
             continue
         dirPaths.append(dirPath)
+        # 是否只获取第一个匹配项
+        if first: break
     return dirPaths
--- src2/peak_eval.py
+++ src2/peak_eval.py
@@ -1,20 +1,20 @@
-from json import load
+import pickle
 import sys
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Union
 import pandas as pd
 
 sys.path.append("..\\")
 from Util.Json import loadJson
-from Util.Path import getFPsByEndwith
+from Util.Path import getFPsByEndwith, getFPsByName
 
-def peakF(merged_df: pd.DataFrame, threshold: int=27, window_size: int=10000) -> int:
+def peakF(merged_df: pd.DataFrame, threshold: int= 27, window_size: int= 2000) -> int:
     """
     使用时域窗口+阈值检测击球波峰，并保存达到条件的击球数据前后一秒的时域数据。
 
     Args:
         merged_df (pd.DataFrame): 包含 gx 的数据框架
         threshold (int, optional): 检测阈值. Defaults to 27.
-        window_size (int, optional): 窗口大小. Defaults to 10000.
+        window_size (int, optional): 窗口大小. Defaults to 4000.
 
     Returns:
         int: 检测到的中值时间戳
@@ -26,7 +26,6 @@ def peakF(merged_df: pd.DataFrame, threshold: int=27, window_size: int=10000) ->
     while i < total_length:
         current_timestamp = merged_df.iloc[i]['unixTimestamp_acc']
 
-        # 定义时域窗口：每个窗口为10秒（10000毫秒）
         start_time = current_timestamp
         end_time = current_timestamp + window_size
 
@@ -42,8 +41,7 @@ def peakF(merged_df: pd.DataFrame, threshold: int=27, window_size: int=10000) ->
 
         # 检查是否有值超过阈值
         if gx_values.max() >= threshold:
-            # peak_value = gx_values.max()
-            peak_index = gx_values.idxmax()
+            peak_index = window_data.index.get_loc(gx_values.idxmax())
             # 返回中间时间戳
             return merged_df.iloc[peak_index]['unixTimestamp_acc']
 
@@ -51,51 +49,135 @@ def peakF(merged_df: pd.DataFrame, threshold: int=27, window_size: int=10000) ->
         i = window_data.index[-1] + 1
     return -1
 
-
-def getNameTimesDict(datasetPath: str) -> Dict[str, List[int]]:
+class DataTimeSet:
     """
-    获取数据名称与击球时间戳列表字典
+    时间戳与原始数据集合
+    """
+    def __init__(self, timeList: List[int], dataFrame: pd.DataFrame) -> None:
+        """
+        初始化
+
+        Args:
+            timeList (List[int]): 时间戳列表
+            dataFrame (pd.DataFrame): 原始数据框架
+        """
+        self.timeList = timeList
+        self.dataFrame = dataFrame
+
+def getNameToDataTimeSetDict(datasetRoot: str, initDataRoot: str, savePath: Union[str, None]= None) -> Union[Dict[str, DataTimeSet], None]:
+    """
+    获取 数据集数据名称 与 时间戳与原始数据集合 字典
 
     Args:
-        datasetPath (str): 数据集路径
+        datasetRoot (str): 数据集根路径
+        initDataRoot (str): 原始数据根路径
+        savePath (Union[str, None], optional): 字典保存路径. Defaults to None.
 
     Returns:
-        Dict[str, List[int]]: 数据名称与击球时间戳列表字典
+        Union[Dict[str, DataTimeSet], None]: 数据集数据名称 与 时间戳与原始数据集合 字典
     """
-    nameTimesDict = {}
-    jsonsPathList = getFPsByEndwith(datasetPath, ".json")
+    nameToDataTimeSetDict = {}
+    # 获取数据集 json 路径列表
+    jsonsPathList = getFPsByEndwith(datasetRoot, ".json")
 
     for jsonsPath in jsonsPathList:
         # 获取 json 列表
         jsons = loadJson(jsonsPath)
         # 记录名称
         recordName = jsons[0]["info"]["recordName"]
-        # 遍历并获取中间的时间戳
-        nameTimesDict[recordName] = [theJson["info"]["startTimestamp"] + 1000 for theJson in jsons]
-        print(nameTimesDict[recordName])
 
-    return nameTimesDict
+        # 遍历并获取中间的时间戳 获取时间戳序列
+        timeList = [theJson["info"]["startTimestamp"] + 1000 for theJson in jsons]
 
-def peakEvalAcc(peakF: Callable[[pd.DataFrame], float], datasetPath: str, initDataPath: str) -> float:
+        # 获取对应原始数据路径
+        initDataPaths = getFPsByName(initDataRoot, recordName, first= True)
+        if len(initDataPaths) != 1:
+            print(f"原始数据异常: {recordName}")
+            return None
+        dataFrame = pd.read_csv(initDataPaths[0])
+
+        # 生成键值对
+        nameToDataTimeSetDict[recordName] = DataTimeSet(timeList, dataFrame)
+        print(f"{recordName} 载入完成")
+
+    # 保存
+    if not savePath is None:
+        with open(savePath, "wb") as file:
+            pickle.dump(nameToDataTimeSetDict, file)
+    return nameToDataTimeSetDict
+
+def loadNameToDataTimeSetDict(loadPath: str) -> Union[Dict[str, DataTimeSet], None]:
+    """
+    载入 数据集数据名称 与 时间戳与原始数据集合 字典
+
+    Args:
+        loadPath (str): 载入路径
+
+    Returns:
+        Union[Dict[str, DataTimeSet], None]: 载入字典
+    """
+    nameToDataTimeSetDict = None
+    with open(loadPath, "rb") as file:
+        nameToDataTimeSetDict = pickle.load(file)
+    return nameToDataTimeSetDict
+
+def peakEvalAcc(peakF: Callable[[pd.DataFrame], float], checkHalfRange: int, nameToDataTimeSetDict: Dict[str, DataTimeSet]) -> float:
     """
     计算检测函数准确率
 
     Args:
         peakF (Callable[[pd.DataFrame], float]): 检测函数
-        datasetPath (str): 数据集路径
-        initDataPath (str): 原始数据路径
+            输入 数据框架
+            生成 中间时间戳
+        checkHalfRange (int): 测试范围半径（ms）
+        nameToDataTimeSetDict (Dict[str, DataTimeSet]): 数据集数据名称 与 时间戳与原始数据集合 字典
 
     Returns:
         float: 准确率
     """
-    # 获取名称与时间戳列表字典
-    nameTimesDict = getNameTimesDict(datasetPath)
+    # 测试总数
+    allNum = 0
+    # 检测总数
+    peakNum = 0
+    # 遍历数据单元
+    for name, dataTimeSetDict in nameToDataTimeSetDict.items():
+        allNum += len(dataTimeSetDict.timeList)
+        print(f"检测 {name}。。。")
+        # 对每个时间戳测试
+        for index, midTimestamp in enumerate(dataTimeSetDict.timeList):
+            # 原始数据框架
+            dataframe = dataTimeSetDict.dataFrame
+            # 起始数据
+            startT = midTimestamp - checkHalfRange
+            endT = midTimestamp + checkHalfRange
+            testDataframe = dataframe[(dataframe['unixTimestamp_acc'] >= startT) & (dataframe['unixTimestamp_acc'] <= endT)]
+            # 检测
+            peakTimestamp = peakF(testDataframe)
+            # 是否在合理范围内
+            if abs(peakTimestamp - midTimestamp) < 500:
+                peakNum += 1
+                print(f"{index} check!")
+        print(f"{name} 检测完成。")
+
+    return float(peakNum) / float(allNum)
 
 DATASET_PATH = r"G:\Badminton\BADS_CLL"
-INIT_DATA_PATH = r"data\processed_fir\merged_files"
+INIT_DATA_PATH = r"..\data\processed_fir\merged_files"
+SAVE_PATH = r"..\src2\theSet\set1.pkl"
+LOAD_PATH = r"..\src2\theSet\set1.pkl"
+LOAD = True
 
 def main() -> None:
-    peakEvalAcc(peakF, DATASET_PATH, INIT_DATA_PATH)
+    # 获取名称与数据时间集合字典
+    nameToDataTimeSetDict = None
+    if not LOAD:
+        nameToDataTimeSetDict = getNameToDataTimeSetDict(DATASET_PATH, INIT_DATA_PATH, SAVE_PATH)
+    else:
+        nameToDataTimeSetDict = loadNameToDataTimeSetDict(LOAD_PATH)
+    if nameToDataTimeSetDict is None:
+        return
+    # 8 秒检测范围
+    print(peakEvalAcc(peakF, 2000, nameToDataTimeSetDict))
 
 if __name__ == "__main__":
     main()
